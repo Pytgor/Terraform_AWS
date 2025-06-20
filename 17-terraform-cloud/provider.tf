e4ea7a62e0ceabeccc9a76fb8fc3a51064d09870
pytgor <pytgor@gmail.com>
2025-06-19 21:04:29 -0400

    terraform Project

---
+++ 17-terraform-cloud/provider.tf
@@ -0,0 +1,24 @@
+terraform { 
+  cloud { 
+    
+    organization = "terraform-kour" 
+
+    workspaces { 
+      name = "terraform-cli" 
+    } 
+  } 
+    required_providers {
+      random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+      }
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+}
+provider "aws" {
+    region = "us-east-1"
+  
+}
